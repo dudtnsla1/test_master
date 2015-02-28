0a04bc7f67f99ce84282a3554bc58ea04624ae53
Sungjin <hsung11@naver.com>
2015-03-01 01:50:01 +0900

    motor

--- TestingBoard/src/testmaster/android/page/FunctionMotorContext.java
+++ TestingBoard/src/testmaster/android/page/FunctionMotorContext.java
@@ -2,10 +2,16 @@ package testmaster.android.page;
 
 import testmaster.android.packet.PacketInfo;
 import testmaster.android.packet.SettingPacket;
+import testmaster.android.testingboard.R;
 import android.content.Context;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class FunctionMotorContext extends FunctionContext{
 
+	Button btn_Send_PID;
+	
 	public FunctionMotorContext(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
@@ -21,6 +27,17 @@ public class FunctionMotorContext extends FunctionContext{
 	@Override
 	public int pageChanged(int pageNum) {
 		// TODO Auto-generated method stub
+		
+		
+		btn_Send_PID = (Button) activity.findViewById(R.id.btn_PID);
+		btn_Send_PID.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				
+
+			}
+		});
 		return 0;
 	}
 
