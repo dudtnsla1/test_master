7741dd2b5784e20287b6b893df45b0d434ad9857
Sungjin <hsung11@naver.com>
2015-03-01 02:21:25 +0900

    commit motor

--- TestingBoard/src/testmaster/android/page/FunctionMotorContext.java
+++ TestingBoard/src/testmaster/android/page/FunctionMotorContext.java
@@ -13,18 +13,18 @@ import android.widget.Toast;
 public class FunctionMotorContext extends FunctionContext implements OnClickListener{
 
 	Button btn_Send_PID;
-	
+
 	private static EditText PID_P_e;
 	private static EditText PID_I_e;
 	private static EditText PID_D_e;
 	private static EditText Object_RPM_e;
-	
+
 	String PID_P;
 	String PID_I;
 	String PID_D;
 	String Object_RPM;
-	
-	
+
+
 	public FunctionMotorContext(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
@@ -40,8 +40,8 @@ public class FunctionMotorContext extends FunctionContext implements OnClickList
 	@Override
 	public int pageChanged(int pageNum) {
 		// TODO Auto-generated method stub
-		
-		
+
+
 		btn_Send_PID = (Button) activity.findViewById(R.id.btn_PID);
 		btn_Send_PID.setOnClickListener(this);
 		return 0;
@@ -55,7 +55,7 @@ public class FunctionMotorContext extends FunctionContext implements OnClickList
 	@Override
 	public void updateChartTemplate() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -63,24 +63,40 @@ public class FunctionMotorContext extends FunctionContext implements OnClickList
 		try{
 			Object_RPM_e=(EditText) activity.findViewById(R.id.object_RPM);
 			Object_RPM = Object_RPM_e.getText().toString();
+			if(Object_RPM.equals(""))
+			{
+				Toast.makeText(activity, "목표 RPM을 입력하세요", Toast.LENGTH_SHORT).show();
+			}
 		}catch(Exception e){
 			Toast.makeText(activity, "목표 RPM을 입력하세요", Toast.LENGTH_SHORT).show();
 		}
 		try{
 			PID_P_e=(EditText) activity.findViewById(R.id.pid_p_Edit);
-			 PID_P = PID_P_e.getText().toString();
+			PID_P = PID_P_e.getText().toString();
+			if(PID_P.equals(""))
+			{
+				Toast.makeText(activity, "P계수를 입력하세요", Toast.LENGTH_SHORT).show();
+			} 
 		}catch(Exception e){
 			Toast.makeText(activity, "P계수를 입력하세요", Toast.LENGTH_SHORT).show();
 		}
 		try{
 			PID_I_e=(EditText) activity.findViewById(R.id.pid_i_Edit);
-			 PID_I = PID_I_e.getText().toString();
+			PID_I = PID_I_e.getText().toString();
+			if(PID_I.equals(""))
+			{
+				Toast.makeText(activity, "I계수를 입력하세요", Toast.LENGTH_SHORT).show();
+			} 
 		}catch(Exception e){
 			Toast.makeText(activity, "I계수를 입력하세요", Toast.LENGTH_SHORT).show();
 		}
 		try{
 			PID_D_e=(EditText) activity.findViewById(R.id.pid_d_Edit);
-			 PID_D = PID_D_e.getText().toString();
+			PID_D = PID_D_e.getText().toString();
+			if(PID_I.equals(""))
+			{
+				Toast.makeText(activity, "D계수를 입력하세요", Toast.LENGTH_SHORT).show();
+			} 
 		}catch(Exception e){
 			Toast.makeText(activity, "D계수를 입력하세요", Toast.LENGTH_SHORT).show();
 		}
