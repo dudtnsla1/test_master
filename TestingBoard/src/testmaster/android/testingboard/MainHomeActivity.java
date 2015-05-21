adf2acc8f3ee76410b525adae15ea41028589fa2
YoungsooAhn <dudtnsla1@naver.com>
2015-05-21 15:42:29 +0900

    Main Function Listener

--- TestingBoard/src/testmaster/android/testingboard/MainHomeActivity.java
+++ TestingBoard/src/testmaster/android/testingboard/MainHomeActivity.java
@@ -14,25 +14,16 @@ import android.bluetooth.BluetoothDevice;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.View.OnGenericMotionListener;
-import android.view.ViewGroup.LayoutParams;
 import android.view.animation.Animation;
-import android.view.animation.Animation.AnimationListener;
-import android.view.animation.ScaleAnimation;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainHomeActivity extends Activity {
 
 	public final static String extraFunctionLayout = "functionLayout";
 	public final static String extraFunctionSettingListener = "settingListener";
-	private final int bluetoothConnectBtnId = R.id.main_home_bluetooth_connect_btn;
 
 	private static final int REQUEST_ENABLE_BT = 0;	
 	private static final int REQUEST_FUNCTION_ACTIVITY = 1;
@@ -47,25 +38,7 @@ public class MainHomeActivity extends Activity {
 			R.id.main_activity2_Motor
 	};
 
-	private int []functionTextIds = new int[] {
-			R.id.main_home_adc_text,
-			R.id.main_home_pwm_text,
-			R.id.main_home_usart_text,
-			R.id.main_home_i2c_text,
-			R.id.main_home_clock_text,
-			R.id.main_home_motor_text
-	};
-
-	private TextView functionTexts[] = new TextView[functionButtonIds.length];
 	private ImageButton functionButtons[] = new ImageButton[functionButtonIds.length];
-	private ImageButton bluetoothConnectBtn;
-	private ImageButton optionBtn;
-	private LinearLayout bluetoothConnectLinearLayout;
-	private LinearLayout functionLinearLayout;
-	private boolean bigBluetoothFlag = true;
-	private LayoutParams bluetoothLayoutParams;
-	private LayoutParams functionLayoutParams;
-	
 
 	private BluetoothAdapter mBluetoothAdapter;
 	private BlueClient blueClient;
@@ -80,46 +53,15 @@ public class MainHomeActivity extends Activity {
 			bluetoothServerObserver.insertObserver();
 			BluetoothObservable.insertDestroyDecorator();
 		}
-		initBluetoothImage();
 		initBluetooth();
 	}
 
-	private void initBluetoothImage() {
-
-		if (bigBluetoothFlag) {
-
-		} else {
-			functionLayoutParams.width -= bluetoothLayoutParams.width;
-			bluetoothLayoutParams.width = bluetoothConnectBtn.getBackground().getBounds().width() * 2;
-			setFunctionTextVisible(View.GONE);
-			bluetoothConnectLinearLayout.setBackgroundColor(getResources().getColor(R.color.main_home_gray_alpha_b));
-
-			bluetoothConnectLinearLayout.setLayoutParams(bluetoothLayoutParams);
-			bluetoothConnectBtn.setBackground(LoadedImage.getImage(R.drawable.image_bluetooth_connect));
-		}
-		bigBluetoothFlag = true;
-	}
-
 	private void initView() {
-		bluetoothConnectBtn = (ImageButton)findViewById(bluetoothConnectBtnId);
-		bluetoothConnectBtn.setOnClickListener(new BluetoothConnectOnClick());
-		bluetoothConnectLinearLayout = (LinearLayout)findViewById(R.id.main_home_bluetooth_connect_layout);
-		bluetoothLayoutParams = bluetoothConnectLinearLayout.getLayoutParams();
-
-		optionBtn = (ImageButton)findViewById(R.id.main_home_option_btn);
-		optionBtn.setOnClickListener(new OptionOnClick());
-
-		functionLinearLayout = (LinearLayout)findViewById(R.id.main_home_funtion_layout);
-		functionLayoutParams = functionLinearLayout.getLayoutParams();
-
-		FunctionTextOnClick funtionTestListener = new FunctionTextOnClick();
-		FunctionImageOnClick functionExplainListener = new FunctionImageOnClick();
+		FunctionOnClick functionExplainListener = new FunctionOnClick();
 
 		for (int i = 0; i < functionButtonIds.length; i++) {
-			functionTexts[i] = (TextView)findViewById(functionTextIds[i]);
-			functionTexts[i].setOnClickListener(funtionTestListener);
 			functionButtons[i] = (ImageButton)findViewById(functionButtonIds[i]);
-			functionButtons[i].setOnGenericMotionListener(functionExplainListener);
+			functionButtons[i].setOnClickListener(functionExplainListener);
 		}
 	}
 
@@ -143,12 +85,6 @@ public class MainHomeActivity extends Activity {
 		setContentView(R.layout.main_home_change);
 		init();
 	}	
-
-	private void setFunctionTextVisible(int visibility) {
-		for (int i = 0; i < functionTexts.length; i++) {
-			functionTexts[i].setVisibility(visibility);
-		}
-	}
 
 	public void superOnbackPressed() {
 		super.onBackPressed();
@@ -196,45 +132,7 @@ public class MainHomeActivity extends Activity {
 		}
 		super.onActivityResult(requestCode, resultCode, data);
 	}
-
-	/**
-	 *  Item listeners */	
-	private class BluetoothResizeAnimationListener implements AnimationListener {
-
-		@Override
-		public void onAnimationEnd(Animation animation) {
-			// TODO Auto-generated method stub			
-			functionLinearLayout.setVisibility(View.VISIBLE);
-		}
-
-		public void setBluetoothLayoutToDeviceInfo() {
-			bluetoothConnectLinearLayout.setBackgroundColor(getResources().getColor(R.color.main_home_emerald_alpha_b));
-		}
-
-		public void setBluetoothLayoutToBluetoothInfo() {
-			bluetoothConnectLinearLayout.setBackgroundColor(getResources().getColor(R.color.main_home_gray_alpha_b));
-		}
-
-		@Override
-		public void onAnimationRepeat(Animation animation) { 
-			// TODO Auto-generated method stub			
-			bluetoothConnectLinearLayout.setLayoutParams(bluetoothLayoutParams);
-			bigBluetoothFlag = !bigBluetoothFlag;
-			if (bigBluetoothFlag) {
-				setFunctionTextVisible(View.GONE);
-				setBluetoothLayoutToBluetoothInfo();
-			} else {
-				setFunctionTextVisible(View.VISIBLE);	
-				setBluetoothLayoutToDeviceInfo();			
-			}
-		}
-
-		@Override
-		public void onAnimationStart(Animation animation) {
-			// TODO Auto-generated method stub
-		}		
-	}
-
+	
 	private class BluetoothConnectOnClick implements OnClickListener {
 		Animation resizeBluetooth = null;
 
@@ -270,11 +168,6 @@ public class MainHomeActivity extends Activity {
 			return blueClient.isConnected();
 		}
 
-		private void showDeviceInfo() {
-			//bluetoothConnectLinearLayout.removeView(findViewById(bluetoothConnectBtnId));
-			
-		}
-
 		@Override
 		public void onClick(View v) {
 			if (resizeBluetooth != null) {
@@ -282,44 +175,12 @@ public class MainHomeActivity extends Activity {
 					return;
 			}
 
-			if (bigBluetoothFlag) {
-				if (!bluetoothConnectVirtual())
-					return;
-				resizeBluetooth = new ScaleAnimation(1, 0f, 1, 1f);
-				bluetoothLayoutParams.width = bluetoothConnectBtn.getBackground().getBounds().width() / 2;
-				functionLayoutParams.width += bluetoothLayoutParams.width; 
-
-				showDeviceInfo();
-
-			} else {
+			if (!bluetoothConnectVirtual())
 				return;
-				/*
-				resizeBluetooth = new ScaleAnimation(1, 0f, 1, 1f);
-				functionLayoutParams.width -= bluetoothLayoutParams.width;
-				bluetoothLayoutParams.width = bluetoothConnectBtn.getBackground().getBounds().width() * 2;
-				*/
-			}
-
-			resizeBluetooth.setDuration(500);
-			resizeBluetooth.setRepeatCount(1);
-			resizeBluetooth.setRepeatMode(ScaleAnimation.REVERSE);
-			resizeBluetooth.setAnimationListener(new BluetoothResizeAnimationListener());
-			functionLinearLayout.setVisibility(View.INVISIBLE);
-
-			bluetoothConnectLinearLayout.startAnimation(resizeBluetooth);
 		}
 	}
 
-	private class FunctionImageOnClick implements OnGenericMotionListener {
-
-		@Override
-		public boolean onGenericMotion(View v, MotionEvent event) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-	}
-
-	private class FunctionTextOnClick implements OnClickListener {
+	private class FunctionOnClick implements OnClickListener {
 
 		@Override
 		public void onClick(View v) {
@@ -327,8 +188,8 @@ public class MainHomeActivity extends Activity {
 			int viewId = v.getId();
 			Intent intent = new Intent(MainHomeActivity.this, MainFunctionActivity.class);
 
-			for (int i = 0; i < functionTextIds.length; i++) {
-				if (viewId == functionTextIds[i]) 
+			for (int i = 0; i < functionButtonIds.length; i++) {
+				if (viewId == functionButtonIds[i]) 
 					intent.putExtra(extraFunctionLayout, MainFunctionActivity.contentLayoutIds[i]);
 			}
 
